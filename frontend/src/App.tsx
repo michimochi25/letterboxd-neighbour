5031a70637b7968404dae996789503e38f80dfc1
Giselle Angela Hanjaya Putri <z5491414@ad.unsw.edu.au>
2025-11-24 14:41:31 -0800

    install tailwind, shadcn

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react';
 import './App.css'
+import { Input } from './components/ui/input';
+import { Button } from './components/ui/button';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [msg, setMsg] = useState<string>("");
+  const [user1, setUser1] = useState<string>("");
+  const [user2, setUser2] = useState<string>("");
+
+  // const getMsg = async () => {
+  //   const response = await fetch('http://localhost:8000/');
+  //   const data = await response.json();
+  //   setMsg(data.message);
+  // }
+
+  // useEffect(() => {
+  //   getMsg();
+  // }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header className="flex flex-col items-center gap-2">
+        <h1 className="font-bold text-6xl text-gray-700">Letterboxd Neighbour</h1>
+        <p className="font-bold">Measure film taste between two Letterboxd users.</p>
+      </header>
+      
+      <section className="mt-6 gap-2 flex flex-col items-center w-full">
+        <Input placeholder="Username 1" className="w-1/2" onChange={
+          (e) => setUser1(e.target.value)
+        }/>
+        <Input placeholder="Username 2" className="w-1/2" onChange={
+          (e) => setUser2(e.target.value)
+        }/>
+        <Button className="w-1/2 bg-gray-700 text-white hover:cursor-pointer hover:bg-gray-700/90">Find Neighbour Films</Button>
+      </section>
     </>
   )
 }
